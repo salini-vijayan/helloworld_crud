3a318ef5fbaf1e42a9e374386c02103dcae1ee14
Salini Vijayan <amsalinivijayan@gmail.com>
2021-11-25 14:40:37 +0530

    crud operation of tasks

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  tasksLists = [];
+  taskName:string ='';
+  @ViewChild('taskInput') input;
 
-  constructor() {}
+  constructor(public alertCtrl : AlertController) {}
+
+  addTasks(){
+    if(this.taskName.length>0){
+      let task = this.taskName;
+      this.tasksLists.push(task);
+      this.taskName = '';
+    }
+  }
+
+  deleteTasks(item_index){
+    this.tasksLists.splice(item_index,1)
+  }
+
+  async updateTasks(item_index){
+    let alert = this.alertCtrl.create({
+      
+      message:'Type in your new task to update.',
+      inputs : [{name: 'editTask', placeholder:'Enter task',value:this.tasksLists[item_index]}],
+      buttons : [{text:'Cancel',role:'cancel'},
+                {text:'Update', handler: data=>{
+                  this.tasksLists[item_index] = data.editTask
+                }}]
+    });
+    (await alert).present();
+  }
 
 }
